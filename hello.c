fd36058bc029f0b3eb921fc8b35c2f079154633c
Jesse Mills <jmills13@unl.edu>
2024-08-29 17:15:45 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jesse Mills
+ *         jmills13@unl.edu
+ * Date: 2024/08/29
+ *
+ * Hack Session Hello program
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jesse Mills\n");
+  return 0;
+  }
