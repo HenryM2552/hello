3ca8dfcd55adf3915954bde8e96367b094adec23
HenryM2552 <hmcqueney2@huskers.unl.edu>
2024-08-29 16:12:45 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Henry McQueney
+ * 8/29/2024
+ * hmcqueney2@huskers.unl.edu
+ * A program that prints my name
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello, my name is Henry McQueney!\n");
+
+  return 0;
+}
